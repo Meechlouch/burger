4b938de3b1daa63955839e3e5fd8461a18af3d45
Dj Meechlouch <Meechlouch@gmail.com>
2021-01-03 23:27:55 -0600

    insertOne method created in orm.js

--- config/orm.js
+++ config/orm.js
@@ -36,4 +36,21 @@ let orm = {
       cb(resp);
     });
   },
+
+  insertOne: function (table, cols, vals, cb) {
+    let queryString = "INSERT INTO " + table;
+    queryString += " (";
+    queryString += cols.toString();
+    queryString += ") ";
+    queryString += "VALUES (";
+    queryString += printQuestionMarks(vals.length);
+    queryString += ") ";
+
+    console.log(queryString);
+
+    connection.query(queryString, vals, (err, resp) => {
+      if (err) throw err;
+      cb(resp);
+    });
+  },
 };
