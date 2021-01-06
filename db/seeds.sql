0a2dcf50d45f9796c707a546821e71b000a799de
Dj Meechlouch <Meechlouch@gmail.com>
2021-01-06 13:02:26 -0600

    devoured burger added to seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,3 +2,7 @@ INSERT INTO burgers (burger_name)
 VALUES ('Turkey Cheeseburger'),
        ('California Burger'),
        ('Tofu Burger');
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Cheeseburger', 1);
+       
