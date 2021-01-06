20d4b66317a7c47ce5d99f38ee20f02933ca352b
Dj Meechlouch <Meechlouch@gmail.com>
2021-01-06 14:26:12 -0600

    removed path

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const PORT = process.env.PORT || 8080;
 const app = express();
-const path = require("path");
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static("public"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
